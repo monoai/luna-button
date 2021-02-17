6ca47e2d0d0d6fa286353ddaba9d9747cf5b5050
Admiy02 <30961614+Admiy02@users.noreply.github.com>
2021-02-18 03:07:09 +0800

    Create ms-MY.js
    Malay translation

---
+++ src/locales/ms-MY.js
@@ -0,0 +1,37 @@
+const ms_MY = {
+    info: {
+        title: "Butang Luna",
+        info: "Maklumat",
+        null: "Kosong",
+        tlHelpers: "Bantuan terjemahan oleh こーでー, Uchan",
+        toGithub: "Sumbangkan terjemahan, bunyi-bunyian atau cadangan lain di Github:",
+        notOfficial: "Laman ini hanyalah buatan peminat serta tiada kaitan dengan hololive secara rasmi.",
+        overlapTips: "Bunyi yang dimainkan secara bertindan tidak boleh dihentikan serta menghasilkan gema. Muatkan semula lama sekiranya sudah puas",
+        loopTips: "Runut yang dimainkan akan terus bergelng selama-lamanya. Akibatnya, dua fungsian yang lain tiada gunanya sehinggalah gelungan ditamatkan",
+        yt_channel: "Saluran Luna",
+        twitter: "Twitter Luna",
+        discord: "Discord Kerajaan Manisan Luna"
+    },
+    action: {
+        toggleNavbar: "Silihkan palang navigasi",
+        close: "tutup",
+        copy: "salin",
+        control: "Kawalan",
+        stopvoice: "Hentikan suara",
+        randomplay: "Suara rawak",
+        overlap: "Suara bertindan",
+        autoplay: "Mainkan terus",
+        loop: "Gelungkan",
+        playing: "Main balik sekarang：",
+        noplay: "Tiada main balik.",
+        volume: "Kelantangan: "
+    },
+    lang: {
+        'zh-CN': "简体中文",
+        'en-US': "English",
+        'ja-JP': "日本語",
+        'zh-TW': "中文"
+        'ms-MY': "bahasa Melayu"
+    }
+}
+export default ms_MY;
